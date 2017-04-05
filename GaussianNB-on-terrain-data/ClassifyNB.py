15c319e1bcc12663ed82a7e2ab37e7dfbc4f2c17
rajukoushik <g.rajukoushik@gmail.com>
2017-04-05 15:50:42 +0530

    GaussianNB on terrain Data

--- GaussianNB-on-terrain-data/ClassifyNB.py
+++ GaussianNB-on-terrain-data/ClassifyNB.py
@@ -1,3 +1,5 @@
+from sklearn.naive_bayes import GaussianNB
+
 def classify(features_train, labels_train):
     ### import the sklearn module for GaussianNB
     ### create classifier
@@ -7,7 +9,7 @@ def classify(features_train, labels_train):
 
     ### your code goes here
     clf = GaussianNB()
-    clf.fit(feature_train, labels_train)
+    clf.fit(features_train, labels_train)
 
     return clf
 
